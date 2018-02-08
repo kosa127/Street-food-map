0dfa102ecba09decb8cc2a3bce3814bbe741a6e5
kosa127 <katel12@o2.pl>
2018-02-08 23:23:49 +0100

    initialized adding markers, changed project structure

--- StreetFood/StreetFood/Form1.cs
+++ StreetFood/StreetFood/Form1.cs
@@ -38,7 +38,7 @@ namespace StreetFood
         private void button1_Click(object sender, EventArgs e)
         {
             //getting values from inputs
-            string city = cityBox.Text;
+            string city = "vancouver"; //cityBox.Text;
             string street = streetBox.Text;
             string zip = zipCodeBox.Text;
 
@@ -50,11 +50,13 @@ namespace StreetFood
             StreamReader reader = new StreamReader(dataStream);
             string data = reader.ReadToEnd();
 
-            this.getVendorsFromApi(data);
-
             reader.Close();
             dataStream.Close();
             response.Close();
+
+            gMap.SetPositionByKeywords(city); //Position = this.getPointByCityName(city);
+            List<Vendor> vendors = this.getVendorsFromApi(data);
+            this.makeMarkers(vendors);
         }
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -66,14 +68,15 @@ namespace StreetFood
             gMap.DragButton = MouseButtons.Left;
             gMap.CanDragMap = true;
             gMap.MapProvider = GMapProviders.GoogleMap;
-            gMap.Position = new PointLatLng(42.75, -97.75);
-            gMap.MinZoom = 0;
+            gMap.SetPositionByKeywords("new york");
+            gMap.MinZoom = 4;
             gMap.MaxZoom = 24;
-            gMap.Zoom = 9;
+            gMap.Zoom = 11;
             gMap.AutoScroll = true;
+            gMap.MarkersEnabled = true;
         }
      
-        private void getVendorsFromApi(string data)
+        private List<Vendor> getVendorsFromApi(string data)
         {
             List<Vendor> vendors = new List<Vendor>();
             dynamic vendorsData = JsonConvert.DeserializeObject(data);
@@ -123,7 +126,32 @@ namespace StreetFood
                 }
             }
 
-            Console.WriteLine(vendors[0].facebook);
+            return vendors;
+        }
+
+        private PointLatLng getPointByCityName(string city) //TODO: should be removed
+        {
+            List<PointLatLng> points;
+            GMapProviders.GoogleMap.GetPoints(city, out points);
+
+            return points[0];
+        }
+
+        private void makeMarkers(List<Vendor> vendors)
+        {
+            Bitmap markerIcon = new Bitmap(Properties.Resources.foodtruck_icon);
+            GMapOverlay markersOverlay = new GMapOverlay("vendorMarkers");
+
+            GMarkerGoogle marker = new GMarkerGoogle(this.getPointByCityName("vancouver"), markerIcon);
+            marker.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marker.ToolTipText = vendors[0].description;
+            marker.ToolTip = new GMapBaloonToolTip(marker);
+            marker.ToolTip.Fill = new SolidBrush(Color.GhostWhite);
+            marker.ToolTip.Stroke = new Pen(Color.Gold);
+
+            markersOverlay.Markers.Add(marker);
+            gMap.Overlays.Add(markersOverlay);
+          
         }
     }
 }
