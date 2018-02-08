c539f102f857ff87142ec02812753619f8e688a6
kacperorzechowski <kacper.orzechowski96@gmail.com>
2018-02-08 12:27:28 +0100

    Parse response from json to c# models (#2)

---
+++ StreetFood/StreetFood/Open.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFood
+{
+    class Open
+    {
+        public int start { get; set; }
+        public int end { get; set; }
+        public string display { get; set; }
+        public float latitude { get; set; }
+        public float longitude { get; set; }
+    }
+}
---
+++ StreetFood/StreetFood/Vendor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetFood
+{
+    class Vendor
+    {
+        public string descriptionShort { get; set; }
+        public string identifier { get; set; }
+        public string name { get; set; }
+        public string region { get; set; }
+        public string url { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public string twitter { get; set; }
+        public string facebook { get; set; }
+        public string instagram { get; set; }
+        public string description { get; set; }
+        public int rating { get; set; }
+        public List<PaymentMethod> paymentMethods { get; set; }
+        public List<Open> opens { get; set; }
+    }
+}
