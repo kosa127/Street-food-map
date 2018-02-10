ca5dc3b3c4fc6c41cb1c51bb08b165382b7d0425
kosa127 <katel12@o2.pl>
2018-02-10 18:51:11 +0100

    Made tooltips to show up on click, only one at the time. Implemented night mode (#5)

--- StreetFood/StreetFood/models/PaymentMethod.cs
+++ StreetFood/StreetFood/models/PaymentMethod.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace StreetFood
+namespace StreetFood.models
 {
     class PaymentMethod
     {
--- StreetFood/StreetFood/models/VendorTooltip.cs
+++ StreetFood/StreetFood/models/VendorTooltip.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Drawing;
 using GMap.NET.WindowsForms.ToolTips;
 using GMap.NET.WindowsForms;
-using System.Drawing;
 using System.Drawing.Drawing2D;
-using System.Windows.Forms;
 
 namespace StreetFood.models
 {
